ab0615aacc7400fcbf26c06f07bf9aa261179e5e
Espen Kørra <ekorra@gmail.com>
2020-04-15 19:30:11 +0200

    Slettet ubrukt kode

--- NextMoveSample/Capabilities.cs
+++ NextMoveSample/Capabilities.cs
@@ -29,15 +29,7 @@ namespace NextMove.Lib
         [JsonProperty("documentTypes")]
         public DocumentType[] DocumentTypes { get; set; }
 
-        public string PrettyProcessName
-        {
-            get
-            {
-                //urn: no: difi: profile: arkivmelding: planByggOgGeodata: ver1.0
-
-                return Process.Split(':')[5];
-            }
-        }
+        public string PrettyProcessName => Process.Split(':')[5];
     }
 
     public partial class DocumentType
@@ -51,25 +43,8 @@ namespace NextMove.Lib
 
     public partial class Capabilities
     {
-        public static Capabilities FromJson(string json) => JsonConvert.DeserializeObject<Capabilities>(json, QuickType.Converter.Settings);
+        public static Capabilities FromJson(string json) => JsonConvert.DeserializeObject<Capabilities>(json);
     }
 
-    public static class Serialize
-    {
-        public static string ToJson(this Capabilities self) => JsonConvert.SerializeObject(self, QuickType.Converter.Settings);
-    }
-
-    internal static class Converter
-    {
-        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
-        {
-            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
-            DateParseHandling = DateParseHandling.None,
-            Converters =
-            {
-                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
-            },
-        };
-    }
 
 }
